cecb57cc61905859bf33c57db598c93f1670e757
Amar <techwithapd@gmail.com>
2024-02-04 21:18:57 +0530

    Updated till 20:41

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from "./src/Login";
+import Detail from "./src/Detail";
+import Dashboard from "./src/Dashboard";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+        name='Login'
+        component={Login}
+        options={{headerShown: false}}
+        />
+        <Stack.Screen
+        name='Detail'
+        component={Detail}
+        options={{headerShown: false}}
+        />
+        <Stack.Screen
+        name='Dashboard'
+        component={Dashboard}
+        options={{headerShown: false}}
+        />        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
